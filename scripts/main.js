128d361f5d480fea60b355768178c23adfd79cd0
Iris <irinaais90@yandex.ru>
2022-03-08 01:27:11 +0300

    feat: сделала открытие и закрытие попапа edit

---
+++ scripts/main.js
@@ -0,0 +1,11 @@
+const popup = document.querySelector(".popup");
+const openPopup = document.querySelector(".popup-open");
+const closePopup = popup.querySelector(".popup__close");
+
+function togglePopup() {
+    popup.classList.toggle("popup__opened");
+  }
+
+openPopup.addEventListener("click", togglePopup);
+
+closePopup.addEventListener("click", togglePopup);
