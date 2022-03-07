99e5f77e5f3d5bb69088da4ed30b1772c9ba2fcc
Iris <irinaais90@yandex.ru>
2022-03-08 01:45:37 +0300

    feat: сделала закрытие попапа edit, если нажимаешь вокруг попапа

--- scripts/main.js
+++ scripts/main.js
@@ -3,8 +3,14 @@ const openPopup = document.querySelector(".popup-open");
 const closePopup = popup.querySelector(".popup__close");
 
 function togglePopup() {
-    popup.classList.toggle("popup__opened");
+  popup.classList.toggle("popup__opened");
+}
+
+popup.addEventListener("click", function (event) {
+  if (event.target === event.currentTarget) {
+      togglePopup();
   }
+});
 
 openPopup.addEventListener("click", togglePopup);
 
