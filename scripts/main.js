d36d029f530fb885eb777d61d6114e03b1c31ecf
Iris <irinaais90@yandex.ru>
2022-03-09 01:46:38 +0300

    feat: сделать попап

--- scripts/main.js
+++ scripts/main.js
@@ -9,7 +9,7 @@ function togglePopup() {
 
 popup.addEventListener("click", function (event) {
   if (event.target === event.currentTarget) {
-      togglePopup();
+    togglePopup();
   }
 });
 
@@ -17,12 +17,18 @@ openPopup.addEventListener("click", togglePopup, true);
 
 closePopup.addEventListener("click", togglePopup, true);
 
-
 // === inputs ===
 const nameUser = document.querySelector(".profile__title");
 const nameUserPopup = popup.querySelector(".name-user");
 const workUser = document.querySelector(".profile__subtitle");
 const workUserPopup = popup.querySelector(".work-user");
+const saveButton = popup.querySelector(".popup__save");
 
 nameUserPopup.value = nameUser.innerText;
 workUserPopup.value = workUser.innerText;
+
+saveButton.addEventListener("click", function () {
+  nameUser.innerText = nameUserPopup.value;
+  workUser.innerText = workUserPopup.value;
+  popup.classList.remove("popup__opened");
+});
