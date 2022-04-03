777bbac48809925f11a7ed99ba0a815687e81608
Iris <irinaais90@yandex.ru>
2022-04-03 16:56:18 +0300

    feat: если input не валидный, то button меняет класс на button_variant_inactive

---
+++ scripts/validationForms.js
@@ -0,0 +1,65 @@
+// ========================= validation forms ========================================
+//показывает элемент ошибки
+const showError = (formElement, inputElement, errorMessage) => {
+  const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+  inputElement.classList.add('popup__input_type_error');
+  errorElement.textContent = errorMessage;
+  errorElement.classList.add('popup__input-error_active');
+};
+//скрывает элемент ошибки
+const hideError = (formElement, inputElement) => {
+  const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+  inputElement.classList.remove('popup__input_type_error');
+  errorElement.classList.remove('popup__input-error_active');
+  errorElement.textContent = '';
+};
+//поверяет валидность формы, вызывает показ или скрытие ошибки
+const checkInputValidity = (formElement, inputElement) => {
+  if (!inputElement.validity.valid) {
+    showError(formElement, inputElement, inputElement.validationMessage);
+  } else {
+    hideError(formElement, inputElement);
+  }
+};
+//слушатель на все input
+const setEventListeners = (formElement) => {
+  const inputList = Array.from(formElement.querySelectorAll('.popup__input'));
+  const buttonElement = formElement.querySelector('.form__submit');
+  // toggleButtonState(inputList, buttonElement);
+  inputList.forEach((inputElement)=> {
+    inputElement.addEventListener('input', function () {
+      checkInputValidity(formElement, inputElement);
+      toggleButtonState(inputList, buttonElement);
+    });
+  });
+};
+//слушатель для всей формы
+const enableValidation = () => {
+  const formList = Array.from(document.querySelectorAll('.popup__form'));
+  formList.forEach((formElement) => {
+    formElement.addEventListener('submit', (evt) => {
+      evt.preventDefault();
+    });
+    setEventListeners(formElement);
+  });
+};
+//ищем невалидный input
+const hasInvalidInput = (inputList) => {
+  return inputList.some((inputElement) => {
+    return !inputElement.validity.valid;
+  });
+};
+//если input не валидный, деактивация button и наоборот
+const toggleButtonState = (inputList, buttonElement) => {
+  if (hasInvalidInput(inputList)) {
+    buttonElement.classList.add('button_variant_inactive');
+    buttonElement.setAttribute("disabled", "disabled");
+  } else {
+    buttonElement.classList.remove('button_variant_inactive');
+    buttonElement.removeAttribute("disabled", "disabled");
+  }
+};
+
+enableValidation();
+
+
